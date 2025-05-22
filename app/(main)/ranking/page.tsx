24a1b21524e9371202f7bdf58a4a60853a125e88
Jairo Freire <jairofreireortiz10@gmail.com>
2025-05-22 18:29:10 -0500

    feat(ranking): create ranking page with position table component

---
+++ app/(main)/ranking/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { TablePosicion } from './components/position_table'
+
+function Page() {
+
+    return (
+        
+            <div className='my-12'>
+                <h1 className="text-4xl font-roboto font-bold text-center text-white mt-10">CLASIFICACION</h1>
+
+                <TablePosicion ></TablePosicion>
+                
+            </div>
+
+    );
+}
+
+export default Page;
